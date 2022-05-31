b8caba44c345ff2dc076867ffdf0f5ed0dc67d26
Wal0798 <106501617+Wal0798@users.noreply.github.com>
2022-05-30 22:40:09 -0500

    Prueba2

--- app/src/main/java/com/enz/app02/MainActivity.java
+++ app/src/main/java/com/enz/app02/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Prueba de trabajo, walter estuvo aqui.
+        // Hola a todos.
     }
 }
