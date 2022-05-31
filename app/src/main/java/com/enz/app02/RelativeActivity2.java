8adb6794fc253169d6580e6c8714ed3d16ed3e1e
Enzo Parisi <106271426+PalomaBroster@users.noreply.github.com>
2022-05-30 22:19:02 -0500

    Prueba

---
+++ app/src/main/java/com/enz/app02/RelativeActivity2.java
@@ -0,0 +1,14 @@
+package com.enz.app02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative2);
+    }
+}
